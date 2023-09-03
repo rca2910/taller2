2f0c81b5ec2987eabddac9b0d7ceb9dc1c96fce9
Rodrigo <rca2910@gmail.com>
2023-09-03 20:58:39 -0300

    usuario

---
+++ src/modelo/Administrador.java
@@ -0,0 +1,11 @@
+
+package modelo;
+
+
+public class Administrador extends Usuario{
+    
+    public Administrador(String cedula, String nombre, String apellido, String contrasena) {
+        super(cedula, nombre, apellido, contrasena);
+    }
+    
+}
---
+++ src/modelo/Lector.java
@@ -0,0 +1,11 @@
+
+package modelo;
+
+
+public class Lector extends Usuario{
+    
+    public Lector(String cedula, String nombre, String apellido, String contrasena) {
+        super(cedula, nombre, apellido, contrasena);
+    }
+    
+}
