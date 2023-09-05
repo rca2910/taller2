0b6f8a04034dc274f7915799fd36e9df9ed1283c
rca2910 <143961178+rca2910@users.noreply.github.com>
2023-09-05 07:41:09 -0300

    52

--- src/modelo/Fila.java
+++ src/modelo/Fila.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
-/**
- *
- * @author rca29
- */
+
 public class Fila {
+    
+    private int id;
+    private String dato;
+    private int numero;
+
+    public Fila(int id, String dato, int numero) {
+        this.id = id;
+        this.dato = dato;
+        this.numero = numero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDato() {
+        return dato;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDato(String dato) {
+        this.dato = dato;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    
     
 }
