2477c06054f2b891325c2f1100f4c1bceb584c29
rca2910 <143961178+rca2910@users.noreply.github.com>
2023-09-05 22:00:44 -0300

    5

--- src/modelo/Tabla.java
+++ src/modelo/Tabla.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
-/**
- *
- * @author rca29
- */
+import java.util.List;
+
+
 public class Tabla {
     
+    private int id;
+    private String nombre;
+    private List columna;
 }
