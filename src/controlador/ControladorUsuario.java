3d5d5504ef4cd8c5dfbc3cd5b303173363d824bb
rca2910 <143961178+rca2910@users.noreply.github.com>
2023-09-12 11:18:44 -0300

    12/09

--- src/controlador/ControladorUsuario.java
+++ src/controlador/ControladorUsuario.java
@@ -36,24 +36,88 @@ public class ControladorUsuario implements IControladorUsuario {
 
     public Mensaje AltaAdministrador(String cedula, String nombre, String apellido, String contrasena) {
 
-        Mensaje respuesta =new Mensaje("", false);
-        if (ObtenerUsuarioxCedula(cedula)!= null){
-            
+        Mensaje respuesta = new Mensaje("", false);
+        if (ObtenerUsuarioxCedula(cedula) != null) {
+
             respuesta.setMensaje("El usuario ya se encuentra en el sistema");
-            
+
             return respuesta;
-            
-        }
-        else {
-            Administrador nuevo = new Administrador (cedula, nombre, apellido, contrasena);
-            
+
+        } else {
+            Administrador nuevo = new Administrador(cedula, nombre, apellido, contrasena);
+
             Sistema.getInstance().getAdministradores().add(nuevo);
-            
+
             respuesta.setMensaje("El usuario fue creado con exito");
             respuesta.setExito(true);
-            
+
             return respuesta;
         }
-        
+
     }
+
+    public Mensaje AltaComun(String cedula, String nombre, String apellido, String contrasena) {
+
+        Mensaje respuesta = new Mensaje("", false);
+        if (ObtenerUsuarioxCedula(cedula) != null) {
+
+            respuesta.setMensaje("El usuario ya se encuentra en el sistema");
+
+            return respuesta;
+
+        } else {
+            Comun nuevo = new Comun(cedula, nombre, apellido, contrasena);
+
+            Sistema.getInstance().getComunes().add(nuevo);
+
+            respuesta.setMensaje("El usuario fue creado con exito");
+            respuesta.setExito(true);
+
+            return respuesta;
+        }
+    }
+
+    public Mensaje AltaLector(String cedula, String nombre, String apellido, String contrasena) {
+
+        Mensaje respuesta = new Mensaje("", false);
+        if (ObtenerUsuarioxCedula(cedula) != null) {
+
+            respuesta.setMensaje("El usuario ya se encuentra en el sistema");
+
+            return respuesta;
+
+        } else {
+            Lector nuevo = new Lector(cedula, nombre, apellido, contrasena);
+
+            Sistema.getInstance().getLectores().add(nuevo);
+
+            respuesta.setMensaje("El usuario fue creado con exito");
+            respuesta.setExito(true);
+
+            return respuesta;
+        }
+    }
+
+    public Mensaje BajaAdministrador(String cedula) {
+
+        Usuario aBuscar = ObtenerUsuarioxCedula(cedula);
+        Mensaje respuesta = new Mensaje("", false);
+        if ((aBuscar) != null) {
+
+            Sistema.getInstance().getAdministradores().remove(aBuscar);
+            Sistema.getInstance().getLectores().remove(aBuscar);
+            Sistema.getInstance().getComunes().remove(aBuscar);
+
+            respuesta.setMensaje("El usuario ha sido eliminado");
+            respuesta.setExito(true);
+            return respuesta;
+        }
+        else {
+            
+            respuesta.setMensaje ("El usuario no existe en el sistema");
+            return respuesta;
+            
+    }
+
+}
 }
